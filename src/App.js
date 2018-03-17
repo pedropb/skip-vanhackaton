358f287ef11901fbd4ce4f1bff9ccabfe81314a4
Pedro Baracho <pedropbaracho@gmail.com>
2018-03-17 10:45:51 -0300

    Implemented Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Cart from './components/Cart';
+import ProductList from './components/ProductList';
+import Product from './components/Product';
+import OrderList from './components/OrderList';
+import Order from './components/Order';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/products" component={ProductList} />
+          <Route path="/product/:productId" component={Product} />
+          <Route path="/orders" component={OrderList} />
+          <Route path="/order/:orderId" component={Order} />
+        </div>
+      </Router>
+
+
     );
   }
 }
