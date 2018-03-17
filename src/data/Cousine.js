3fc487d3cf0d3afde4fb747d1f6cab2d5fff762b
Pedro Baracho <pedropbaracho@gmail.com>
2018-03-17 11:39:57 -0300

    Adding dummy data

---
+++ src/data/Cousine.js
@@ -0,0 +1,19 @@
+export default 
+[
+    {
+        "id": 1,
+        "name": "Chinese"
+    },
+    {
+        "id": 2,
+        "name": "Pizza"
+    },
+    {
+        "id": 3,
+        "name": "Vietnamese"
+    },
+    {
+        "id": 4,
+        "name": "Sushi"
+    }
+];
---
+++ src/data/Order.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        "id": 0,
+        "date": "2018-03-17T14:36:55.489Z",
+        "customerId": 0,
+        "deliveryAddress": "string",
+        "contact": "string",
+        "storeId": 0,
+        "orderItems": [
+            {
+                "id": 0,
+                "orderId": 0,
+                "productId": 0,
+                "product": {
+                    "id": 0,
+                    "storeId": 0,
+                    "name": "string",
+                    "description": "string",
+                    "price": 0
+                },
+                "price": 0,
+                "quantity": 0,
+                "total": 0
+            }
+        ],
+        "total": 0,
+        "status": "string",
+        "lastUpdate": "2018-03-17T14:36:55.489Z"
+    }
+];
